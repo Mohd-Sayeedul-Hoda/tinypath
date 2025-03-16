6323b9c9f7222c9863389d0c1366e497a080e7c8
Sayeed <sayeedulhoda@gmail.com>
2025-03-16 12:44:21 +0530

    add json logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,11 +5,35 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/signal"
+
+	jsonlog "github.com/Mohd-Sayeedul-Hoda/url_shortner/internal/jsonLog"
 )
 
 func main() {
 	ctx := context.Background()
-	err := run(ctx, os.Stdin)
+
+	isTest := os.Getenv("GO_TEST") != ""
+	var getenv func(string) string
+
+	if isTest {
+		// for testing we can set enviroment variable
+		getenv = func(key string) string {
+			switch key {
+			case "MYAPP_FORMAT":
+				return "markdown"
+			case "MYAPP_TIMEOUT":
+				return "5s"
+			default:
+				return ""
+			}
+		}
+	} else {
+		getenv = func(key string) string {
+			return os.Getenv(key)
+		}
+	}
+	err := run(ctx, getenv, os.Stdin)
 	if err != nil {
 		fmt.Fprintf(os.Stdin, "%s\n", err)
 		os.Exit(1)
@@ -17,7 +41,12 @@ func main() {
 
 }
 
-func run(ctx context.Context, w io.Writer) error {
+func run(ctx context.Context, getenv func(string) string, w io.Writer) error {
+
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	logger := jsonlog.New(w, jsonlog.LevelInfo)
 
 	return nil
 }
